4ca6fe38500b398e63d0890f3b44452fb9064248
imoleFolio <dremkay71@gmail.com>
2024-11-08 18:15:07 +0100

    hero almost done

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
+const header = document.getElementById("header")
+
+function updateHeight() {
+    header.style.setProperty('--header-height', `${header.offsetHeight}px`); 
+}
+
+window.addEventListener("resize", updateHeight);
+updateHeight();
+
 
 const hbar = document.getElementById("hbar")
 const xbar = document.getElementById("xbar")
@@ -9,3 +18,25 @@ function handleMenu(){
     xbar.classList.toggle("hidden")
     mMenu.classList.toggle("hidden")
 }
+
+const images = [
+    "fist_knuckles.jpeg",
+    "girl_eng.jpeg",
+    "girl_grad_happy.jpeg",
+    "girl_grad.jpeg",
+    "girls_cap.jpeg",
+    "lab.jpeg",
+    "presentation.jpeg",
+    "students_balloons.jpeg"
+]
+
+
+let index = 0;
+
+function changeBackground() {
+    const hero = document.getElementById("hero")
+    hero.style.backgroundImage = `url('/public/img/${images[index]}')`
+    index = (index+1) % images.length;
+}
+
+setInterval(changeBackground, 3000)
