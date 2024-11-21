e01e1223ea8cceaed66c1fce5aa57eec1a81f4c0
imoleFolio <dremkay71@gmail.com>
2024-11-21 13:40:00 +0100

    dashboard started

--- routes/dashboard.go
+++ routes/dashboard.go
@@ -1,10 +1,18 @@
 package routes
 
 import (
+	"html/template"
 	"io"
+	"log"
 	"net/http"
 )
 
 func Dashboard(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>Dashboard Page<h1>")
+	tmpl, err := template.ParseFiles("templates/dashboard/base.html")
+	if err != nil {
+		io.WriteString(w, err.Error())
+		log.Println(err)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
